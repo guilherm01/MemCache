7d96915bc3dc3da2bdcbcb51e9deb01d131c64b4
CallMeLink <celso.jr25@hotmail.com>
2024-05-20 22:41:25 -0300

    constantes definidas

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void){
-    printf("Hello World");
-    printf("Celso Russomano");
-}
+//Definindo as constantes
+
+#define MAX_CACHE 32 * 1024 //32KB
+#define MAX_MP 256 * 1024 //256KB
+#define TAM_PALAVRA 4 //4 Bytes
+#define PALAVRA_MAX_BLOCO 8
