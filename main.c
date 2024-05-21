708efe6de9466c02230d34ffe794fa1c5523ecb4
CallMeLink <celso.jr25@hotmail.com>
2024-05-20 23:06:01 -0300

    estrutura de dados e lógica pronta junto com uma todo list no final do código

--- main.c
+++ main.c
@@ -8,3 +8,31 @@
 #define MAX_MP 256 * 1024 //256KB
 #define TAM_PALAVRA 4 //4 Bytes
 #define PALAVRA_MAX_BLOCO 8
+
+typedef struct linhaCache
+{
+    int *palavras;
+    int validacao; //Variável que vou usar para verificar se o bloco da cache é pode realmente ser utilizado.
+    int tag;
+    int contLfu;
+};
+
+typedef struct memoriaPrincipal
+{
+    int **blocos;
+    int tamanhoBloco;
+    int numeroBlocos;
+};
+
+
+typedef struct cache
+{
+    int numConjuntos;
+    int linhasPorConjunto;
+};
+
+//To do
+//Fazer o código de leitura do arquivo de entrada.
+//Fazer o código para iniciar a memória principal e a cache.
+//Fazer as funções de mapeamento e substituição.
+//Interface gráfica que interage com o usuário.
