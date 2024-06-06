eb7d916a40453130cc630316b55099d7be4a2716
guilherm01 <gamagomesg05@gmail.com>
2024-06-05 22:00:49 -0400

    Definição das struct e inicialização da cache. Codigo comentado. Sugiro a gente entrar com um endereço decimal e manipular os bits para encontrar os indices.

--- main.c
+++ main.c
@@ -1,38 +1,54 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <math.h>
+#include <stdbool.h>
 
-//Definindo as constantes
+typedef struct{
+    unsigned int tagBit; //Bit de tag;
+    unsigned int conjBit; //Bit de conjunto;
+    unsigned int blocoBit; //Bit de bloco/celula/palavra 
+    bool vBit; //Bit de validade;
+}Linha; 
 
-#define MAX_CACHE 32 * 1024 //32KB
-#define MAX_MP 256 * 1024 //256KB
-#define TAM_PALAVRA 4 //4 Bytes
-#define PALAVRA_MAX_BLOCO 8
+typedef struct{
+    Linha *linhas; //Linhas do conjunto. O ponteiro é necessario para criar um vetor dinamicamente
+}Conj; 
 
-typedef struct linhaCache
-{
-    int *palavras;
-    int validacao; //Variável que vou usar para verificar se o bloco da cache é pode realmente ser utilizado.
-    int tag;
-    int contLfu;
-};
+typedef struct{
+    Conj *conjunto; //Conjuntos da cache. O ponteiro é necessario para criar um vetor dinamicamente
+    unsigned int mcTam; //Tamanho da cache
+    unsigned int linhasPorConj; //Linhas por conj
+    int miss; //Endereco nao esta na cache
+    int hit;  //Endereco esta na cache
+}Cache;
 
-typedef struct memoriaPrincipal
-{
-    int **blocos;
-    int tamanhoBloco;
-    int numeroBlocos;
-};
+Cache* cache_init(unsigned int mcTam, unsigned int linhasPorConj){
+        int i, j;
+        unsigned int conjQnt = mcTam/linhasPorConj; //Quantidade de conjuntos;
+        Cache *cache = malloc(sizeof(Cache)); //Alocando memoria dinamicamente para cache;
+        cache->conjunto = malloc(conjQnt*sizeof(Conj)); //Criando um vetor conjunto dinamicamente em função da quantidade de conjuntos;
+        cache->mcTam = mcTam;
+        cache->linhasPorConj = linhasPorConj;
+        int miss = 0;
+        int hit = 0;
+
+        for(i = 0; i < conjQnt; i++){
+            cache->conjunto[i].linhas = malloc(linhasPorConj*sizeof(Linha)); //Criando um vetor linha dinamicamente em função da quantidade de linhasPorConjunto;
+            for(j = 0; j < linhasPorConj; j++){
+                cache->conjunto[i].linhas[j].tagBit = 0;
+                cache->conjunto[i].linhas[j].blocoBit = 0;
+                cache->conjunto[i].linhas[j].conjBit = 0;
+                cache->conjunto[i].linhas[j].vBit = false;
+            }
+        }   
+        return cache; 
+}
+
+//Vamos manipular bits - deslocamento (<< && >>) - para encontrar os indices de cada endereco (tag, conjunto, bloco). 
+
+int main(){
 
 
-typedef struct cache
-{
-    int numConjuntos;
-    int linhasPorConjunto;
-};
-
-//To do
-//Fazer o código de leitura do arquivo de entrada.
-//Fazer o código para iniciar a memória principal e a cache.
-//Fazer as funções de mapeamento e substituição.
-//Interface gráfica que interage com o usuário.
+    return 0;
+}
